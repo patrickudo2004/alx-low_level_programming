4bf7d9fb59b7c5b2b06288b6c7736967e11d97d9
Patrick Udoh <patrickudo2004@gmail.com>
2023-02-16 16:10:40 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ *
+ * return (1)
+ */
+
+int main(void)
+{
+	write("and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
