0d139df09f2347c1b2a7ae71b1571606349fd2ef
Patrick Udoh <patrickudo2004@gmail.com>
2023-03-08 14:06:55 -0800

    updated

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _puts_recursion - function like puts();
  * @s: input
@@ -8,7 +7,7 @@
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
-		_putchar('\n');
+	_putchar('\n');
 	else
 	{
 		_putchar(*s);
