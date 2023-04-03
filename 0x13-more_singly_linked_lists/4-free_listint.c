0bb5df222beda5f45927090e876da3018822e82d
Patrick Udoh <patrickudo2004@gmail.com>
2023-04-03 16:35:48 -0700

    updted

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -9,14 +9,8 @@ void free_listint(listint_t *head)
 
 	if (head == NULL)
 	{
-		return;
+    		return;
 	}
-	while (head != NULL)
-	{
-		listint_t *tmp;
-
-		tmp = head;
-		free(tmp);
-		head = head->next;
-	}
+	free_listint(head->next);
+	free(head);
 }
