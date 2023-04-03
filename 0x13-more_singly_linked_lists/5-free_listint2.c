73e15e44274d1e84e4597944cbfb0c1223e5b115
Patrick Udoh <patrickudo2004@gmail.com>
2023-04-03 16:50:21 -0700

    updated

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,7 +1,10 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
 /**
  * free_listint2 - Free nodes and list and set head to null
- * @head: This is my struct
+ * @head: pointer
  */
 void free_listint2(listint_t **head)
 {
@@ -10,13 +13,10 @@ void free_listint2(listint_t **head)
 	{
 		return;
 	}
-	while ((*head) != NULL)
+	while (*head != NULL)
 	{
-		listint_t *tmp;
-
-		tmp = (*head);
-		free(tmp);
+		listint_t *tmp = *head;
 		(*head) = (*head)->next;
+		free(tmp);
 	}
-	(*head) = NULL;
 }
